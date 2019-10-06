1db93c37b8ab836c015a7e71943338acdb9ee40c
rahul_gautam <rahulgautam297@gmail.com>
2019-10-06 20:21:10 +0530

    feat: complete backend

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import mongodb from 'mongodb';
 import { ObjectId } from 'mongodb';
+import cors from 'cors';
 
 const url = "mongodb://localhost:27017/scheduler";
 let mongoDb = null;
@@ -11,22 +12,30 @@ mongodb.connect(url, (err, db) => {
 let express = require('express'),
 app = express();
 app.use(express.json());
+app.use(cors());
 const port = 4000;
 app.listen(port, () => {
 });
 
 app.post('/user-tasks', (req, res) => {
-  mongoDb.collection('users').find({ userName: req.body.userName }).toArray(function(err, results) {
-    if (results.length === 0) {
-      mongoDb.collection('users').insertOne(req.body, (err, result) => {
-        if (err) return console.log(err)
-        res.sendStatus(200);
+  mongoDb.collection('users').findOne({ userName: req.body.userName }, (err, user) => {
+    if (err) return console.log(err);
+    if (user === null) {
+      mongoDb.collection('users').insertOne({ userName: req.body.userName }, (err, createdUser) => {
+        if (err) return console.log(err);
+        mongoDb.collection('projects').find().toArray((error, projects) => {
+          if (error) return console.log(error);
+          res.status(200).send({tasks: [ ], projects: projects, user: createdUser});
+        });
       });
     } else {
-      mongoDb.collection('tasks').find({ userName: req.body.userName, timeStamp: req.body.timeStamp }).toArray(function(err, results) {
+      mongoDb.collection('tasks').find({ userName: req.body.userName, timeStamp: req.body.timeStamp }).toArray((err, results) => {
         // console.log(ObjectId(results[0]._id).getTimestamp());
-        if (err) return console.log(err)
-        res.status(200).send(results);
+        if (err) return console.log(err);
+        mongoDb.collection('projects').find().toArray((error, projects) => {
+          if (error) return console.log(error);
+          res.status(200).send({tasks: results, projects: projects, user: user});
+        });
       });
     }
   });
@@ -35,22 +44,29 @@ app.post('/user-tasks', (req, res) => {
 
 app.post('/save-task', (req, res) => {
   mongoDb.collection('tasks').insertOne(req.body, (err, result) => {
-    if (err) return console.log(err)
+    if (err) return console.log(err);
+    res.status(200).send(result);
+  });
+});
+
+app.post('/add-project', (req, res) => {
+  mongoDb.collection('projects').insertOne(req.body, (err, result) => {
+    if (err) return console.log(err);
     res.status(200).send(result);
   });
 });
 
 app.put('/update-task', (req, res) => {
-  mongoDb.collection('tasks').updateOne({ _id: ObjectId(req.body.taskId) }, {$set:
+  mongoDb.collection('tasks').updateOne({ _id: ObjectId(req.query.taskId) }, {$set:
                         { timerStart: req.body.timerStart, timerEnd: req.body.timerEnd }}, (err, result) => {
-    if (err) return console.log(err)
+    if (err) return console.log(err);
     res.status(200).send(result);
   });
 });
 
 app.get('/previous-tasks', (req, res) => {
-  mongoDb.collection('tasks').find({ userName: req.body.userName, timeStamp: req.body.timeStamp }).toArray(function(err, results) {
-    if (err) return console.log(err)
+  mongoDb.collection('tasks').find({ userName: req.body.userName, timeStamp: req.body.timeStamp }).toArray((err, results) => {
+    if (err) return console.log(err);
     res.status(200).send(result);
   });
 });
